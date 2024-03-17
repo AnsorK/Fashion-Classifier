eec17700dab0ce83bdaed3fe7a1fc229a3ad9184
Ansor Kasimov <ansork02@gmail.com>
2024-03-16 21:01:37 -0400

    Download and store the Fashion MNIST data

--- model.py
+++ model.py
@@ -5,3 +5,56 @@ import os
 import requests
 from sklearn.neighbors import KNeighborsClassifier
 
+'''
+Download and store data from the
+Fashion MNIST dataset
+'''
+
+def download_fashion_mnist(url, file_name):
+    '''
+    Download data from the 'url' and save
+    it in the directory as 'file_name'
+    '''
+
+    if not os.path.exists(file_name):
+        r = requests.get(url)
+        with open(file_name, 'wb') as f:
+            f.write(r.content)
+
+def load_fashion_mnist(image_file, label_file):
+    '''
+    Read in the binary 'image_file' and
+    'label_file' data as NumPy arrays
+    '''
+
+    with gzip.open(image_file, 'rb') as f:
+        images = np.frombuffer(f.read(), np.uint8, offset=16).reshape(-1, 28, 28)
+
+    with gzip.open(label_file, 'rb') as f:
+        labels = np.frombuffer(f.read(), np.uint8, offset=8)
+
+    return images, labels
+
+image_url = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-images-idx3-ubyte.gz'
+label_url = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/train-labels-idx1-ubyte.gz'
+
+image_file = 'train-images-idx3-ubyte.gz'
+label_file = 'train-labels-idx1-ubyte.gz'
+
+download_fashion_mnist(image_url, image_file)
+download_fashion_mnist(label_url, label_file)
+
+train_images, train_labels = load_fashion_mnist(image_file, label_file)
+
+test_image_url = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-images-idx3-ubyte.gz'
+test_label_url = 'http://fashion-mnist.s3-website.eu-central-1.amazonaws.com/t10k-labels-idx1-ubyte.gz'
+
+test_image_file = 't10k-images-idx3-ubyte.gz'
+test_label_file = 't10k-labels-idx1-ubyte.gz'
+
+download_fashion_mnist(test_image_url, test_image_file)
+download_fashion_mnist(test_label_url, test_label_file)
+
+test_images, test_labels = load_fashion_mnist(test_image_file, test_label_file)
+
+class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat', 'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
